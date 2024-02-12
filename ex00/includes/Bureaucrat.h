7e1e8aba9b70195bc9d4f4556ed1d02d583e426e
nsainto <nsainton@student.42.fr>
2024-02-12 17:46:06 +0100

    feat(Bureaucrat): :construction: Finished implementing Bureaucrat class :construction:

--- ex00/includes/Bureaucrat.h
+++ ex00/includes/Bureaucrat.h
@@ -1,15 +1,27 @@
 #ifndef __BUREAUCRAT_H__
 # define __BUREAUCRAT_H__
 # include <string>
+# include <exception>
+# define TOOHIGH "The grade required is too high. The sky is out of reach for you my G"
+# define TOOLOW "The grade required is too low. Hitting rock bottom won't lead you nowhere"
 
 class	Bureaucrat{
-	const std::string	_name;
-	unsigned int		_grade;
+	const std::string		_name;
+	unsigned int			_grade;
 
 	public:
-		Bureaucrat();
+		Bureaucrat( const std::string & name, unsigned int grade );
 		Bureaucrat( const Bureaucrat & );
 		Bureaucrat& operator=( const Bureaucrat & );
 		virtual ~Bureaucrat();
+		class	GradeTooHighException : public std::exception {
+			virtual const char * what() const throw();
+		};
+		class	GradeTooLowException : public std::exception {
+			virtual const char * what() const throw();
+		};
+		unsigned int		getGrade() const ;
+		const std::string &	getName() const ;
 };
+
 #endif
--- ex00/sources/Bureaucrat.cpp
+++ ex00/sources/Bureaucrat.cpp
@@ -2,25 +2,41 @@
 #include <iostream>
 #include <string>
 
-Bureaucrat::Bureaucrat( const std::string & name, const unsigned int grade){
-	this->_name = name;
-	this->_grade = grade;
+Bureaucrat::Bureaucrat( const std::string & name, const unsigned int grade ) : _name(name), _grade(grade){
 	std::clog << "Bureaucrat Default Constructor Called" << std::endl;
+	if (grade < 1)
+		throw (GradeTooHighException());
+	if (grade > 150)
+		throw (GradeTooLowException());
 }
 
-Bureaucrat::Bureaucrat( const Bureaucrat & other ){
+Bureaucrat::Bureaucrat( const Bureaucrat & other ) : _name(other._name), _grade(other._grade){
 	std::clog << "Bureaucrat Copy Constructor Called" << std::endl;
-	this->_name = other._name;
-	this->_grade = other._grade;
 }
 
 Bureaucrat&	Bureaucrat::operator=( const Bureaucrat & other ){
 	std::clog << "Bureaucrat Copy Assignment Operator Called" << std::endl;
-	this->_name = other._name;
+	const_cast<std::string &>(this->_name) = other._name;
 	this->_grade = other._grade;
 	return (*this);
 }
 
 Bureaucrat::~Bureaucrat(){
 	std::clog << "Bureaucrat Destructor Called" << std::endl;
+}
+
+const char *	Bureaucrat::GradeTooHighException::what() const throw(){
+	return TOOHIGH;
+}
+
+const char *	Bureaucrat::GradeTooLowException::what() const throw(){
+	return TOOLOW;
+}
+
+unsigned int	Bureaucrat::getGrade() const {
+	return (this->_grade);
+}
+
+const std::string &	Bureaucrat::getName() const {
+	return (this->_name);
 }
