102c8e4ce735ad734171a8bb38c36f1fba53cfd0
nsainto <nsainton@student.42.fr>
2024-02-13 18:16:34 +0100

    feat(Form): :sparkles: Finished implementation of form class. Needs testing :sparkles:

---
+++ ex01/includes/Form.h
@@ -0,0 +1,33 @@
+#ifndef __FORM_H__
+# define __FORM_H__
+# include <string>
+# include <exception>
+# define FTOOLOW "Where you think you goin' with that fake ass grades. This is too low, get out!"
+# define FTOOHIGH "Who the hell you think this is? A goddamn golden ticket? No such high grades here"
+
+class	Form{
+	const std::string	_name;
+	bool				_signed;
+	const unsigned int	_sign_grade;
+	const unsigned int	_exec_grade;
+
+	public:
+		Form( const std::string & name, const unsigned int sign_grade, \
+		const unsigned int exec_grade );
+		Form( const Form & );
+		Form& operator=( const Form & );
+		virtual ~Form();
+		class	GradeTooHighException : public std::exception{
+			virtual const char * what() const throw();
+		};
+		class	GradeTooLowException : public std::exception{
+			virtual const char * what() const throw();
+		};
+		const std::string & getName() const;
+		bool				isSigned() const;
+		unsigned int		getSignGrade() const;
+		unsigned int		getExecGrade() const;
+};
+
+std::ostream &	operator<<(std::ostream & os, const Form & b);
+#endif
---
+++ ex01/sources/Form.cpp
@@ -0,0 +1,62 @@
+#include "Form.h"
+#include <iostream>
+
+Form::Form( const std::string & name, const unsigned int sign_grade, \
+const unsigned int exec_grade ) : _name(name), _signed(false), _sign_grade(sign_grade), \
+_exec_grade(exec_grade){
+	std::clog << "Form Default Constructor Called" << std::endl;
+	if (sign_grade < 1 || exec_grade < 1)
+		throw (GradeTooHighException());
+	if (sign_grade > 150 || exec_grade > 150)
+		throw (GradeTooLowException());
+}
+
+Form::Form( const Form & other ) : _name(other._name), _signed(other._signed), \
+_sign_grade(other._sign_grade), _exec_grade(other._exec_grade){
+	std::clog << "Form Copy Constructor Called" << std::endl;
+}
+
+Form&	Form::operator=( const Form & other ){
+	std::clog << "Form Copy Assignment Operator Called" << std::endl;
+	_signed = other._signed;
+	const_cast<unsigned int &>(this->_sign_grade) = other._sign_grade;
+	const_cast<unsigned int &>(this->_exec_grade) = other._exec_grade;
+	const_cast<std::string &>(this->_name) = other._name;
+	return (*this);
+}
+
+Form::~Form(){
+	std::clog << "Form Destructor Called" << std::endl;
+}
+
+const char *	Form::GradeTooLowException::what() const throw(){
+	return (FTOOLOW);
+}
+
+const char *	Form::GradeTooHighException::what() const throw(){
+	return (FTOOHIGH);
+}
+
+const std::string &	Form::getName() const{
+	return (this->_name);
+}
+
+bool			Form::isSigned() const{
+	return (this->_signed);
+}
+
+unsigned int	Form::getSignGrade() const{
+	return (this->_sign_grade);
+}
+
+unsigned int	Form::getExecGrade() const{
+	return (this->_exec_grade);
+}
+
+std::ostream &	operator<<(std::ostream & os, const Form & f){
+	os << "Form : " << f.getName();
+	os << "\nSigned ? " << f.isSigned();
+	os << "\nSign Grade : " << f.getSignGrade();
+	os << "\nExec Grade : " << f.getExecGrade() << std::endl;
+	return (os);
+}
