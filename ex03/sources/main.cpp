c28e211098cfdaebc182299859e053d5fc011f4f
nsainto <nsainton@student.42.fr>
2024-02-22 16:28:30 +0100

    refactor(main): :coffin: Removed old code for testing :coffin:

--- ex03/sources/main.cpp
+++ ex03/sources/main.cpp
@@ -142,39 +142,5 @@ int	main(void)
 	check_form("Bill", 95, "Salut");
 	check_form("Patrick", 10, "Bonjour");
 	check_form_loop();
-	/*
-	check_form("Groceries list", 120, 168);
-	check_form("Groceries list", 168, 120);
-	check_form("Chores", 0, 1);
-	check_form("Chores", 1, 0);
-	AForm	b("Bonjour", 2, 4);
-	std::cout << b << std::endl;
-	AForm	c("Salut", 1, 2);
-	std::cout << c << std::endl;
-	AForm	d(c);
-	std::cout << d << std::endl;
-	AForm	e(c);
-	e = b;
-	std::cout << e << std::endl;
-	*/
-	/*
-	int	i(0);
-	while ( i < 2 )
-	{
-		std::cout << "This is the index : " << i << "\nthis is the bureaucrat : " << b << std::endl;
-		try
-		{
-			std::cout << "Trying to upgrade bureaucrat : " << RED << b.getName() << "\e[0m" << std::endl;
-			b.UpGrade();
-		}
-		catch (std::exception & e)
-		{
-			std::cout << e.what() << std::endl;
-		}
-		++i;
-	}
-	std::cout << "This is the index : " << i << " and this is the bureaucrat : " << b << std::endl;
-	*/
-	//check_form_loop();
 	return (0);
 }
